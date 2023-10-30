801e3a8c1bf9ae951d3e3ebc0eb9939ecbb4c642
Kumud Kishor <kumudkishor1901@gmail.com>
2023-10-30 20:00:02 +0530

    manual validation tight coupling

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,11 +10,37 @@ export default class ProductController{
     }
 
     addNewForm(req,res){
-        return res.render('new-product');
+        return res.render('new-product',{errorMessage:null});
     };
 
     addProduct(req,res){
         console.log(req.body);
+
+        // server side form validation 
+
+        const {name,price,imageUrl} = req.body;
+        let errors = [];
+        // validating name
+        if(!name || name.trim()==''){
+            errors.push('Name is invalid');
+        }
+
+        // validating price 
+        if(!price || parseFloat(price)< 0){
+            errors.push('Price is invalid');
+        }
+        // validating url
+        try{
+            const validUrl = new URL(imageUrl);
+        }catch(err){
+            errors.push('URL is invalid');
+        }
+
+        if(errors.length > 0){
+            return res.render('new-product',{errorMessage : errors[0]});
+        }
+         
+
         ProductModel.add(req.body);
         let products = ProductModel.get();
         return res.render('products',{products});
